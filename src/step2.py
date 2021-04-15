a77a8d9224a548bb7ac2b341eec62ebfc17e1b09
amiroo <amir.m.raei@gmail.com>
2021-04-16 01:18:22 +0430

    step2

--- src/step2.py
+++ src/step2.py
@@ -19,12 +19,12 @@ def multiply_mtx(w, mtx, b):
 
 
 # Reading The Train Set
-train_images_file = open('samples/train-images-idx3-ubyte', 'rb')
+train_images_file = open('/Users/amiroo/Desktop/Handwritten-Digit-Recognition/samples/train-images-idx3-ubyte', 'rb')
 train_images_file.seek(4)
 num_of_train_images = int.from_bytes(train_images_file.read(4), 'big')
 train_images_file.seek(16)
 
-train_labels_file = open('samples/train-labels-idx1-ubyte', 'rb')
+train_labels_file = open('/Users/amiroo/Desktop/Handwritten-Digit-Recognition/samples/train-labels-idx1-ubyte', 'rb')
 train_labels_file.seek(8)
 
 train_set = []
@@ -41,10 +41,10 @@ for n in range(num_of_train_images):
 
 
 # Reading The Test Set
-test_images_file = open('samples/t10k-images-idx3-ubyte', 'rb')
+test_images_file = open('/Users/amiroo/Desktop/Handwritten-Digit-Recognition/samples/t10k-images-idx3-ubyte', 'rb')
 test_images_file.seek(4)
 
-test_labels_file = open('samples/t10k-labels-idx1-ubyte', 'rb')
+test_labels_file = open('/Users/amiroo/Desktop/Handwritten-Digit-Recognition/samples/t10k-labels-idx1-ubyte', 'rb')
 test_labels_file.seek(8)
 
 num_of_test_images = int.from_bytes(test_images_file.read(4), 'big')
@@ -64,13 +64,13 @@ for n in range(num_of_test_images):
 
 
 # Second step
-w1 = np.random.normal(loc=0, scale=1, size=(16, 28*28))
-w2 = np.random.normal(loc=0, scale=1, size=(16, 16))
-w3 = np.random.normal(loc=0, scale=1, size=(10, 16))
+w0 = np.random.normal(loc=0, scale=1, size=(16, 28*28))
+w1 = np.random.normal(loc=0, scale=1, size=(16, 16))
+w2 = np.random.normal(loc=0, scale=1, size=(10, 16))
 
+b0 = np.zeros((16,1))
 b1 = np.zeros((16,1))
-b2 = np.zeros((16,1))
-b3 = np.zeros((10,1))
+b2 = np.zeros((10,1))
 
 accuracy = 0
 
@@ -79,9 +79,9 @@ for i in range(0, 100):
     for j in range(28*28):
         main_mtx.append(train_set[i][0][j]) 
     main_mtx = np.asarray(main_mtx)
-    mtx2 = sigmoid(multiply_mtx(w1, main_mtx, b1))
-    mtx3 = sigmoid(multiply_mtx(w2, mtx2, b2))
-    f_mtx = sigmoid(multiply_mtx(w3, mtx3, b3))
+    mtx1 = sigmoid(multiply_mtx(w0, main_mtx, b0))
+    mtx2 = sigmoid(multiply_mtx(w1, mtx1, b1))
+    f_mtx = sigmoid(multiply_mtx(w2, mtx2, b2))
     
     max_value = np.max(f_mtx)
     index_max_value = np.argmax(f_mtx)
